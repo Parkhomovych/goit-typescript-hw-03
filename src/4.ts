d891c4be8d6485f9bfcdffd2c4e22e60b750a8f4
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-11-29 21:09:03 +0200

    comit first

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,38 @@
+class Key {
+    private signature: number = Math.random()
+    getSignature(): number {
+        return this.signature
+    }
+}
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key {
+        return this.key
+    }
+}
+abstract class House {
+    protected door: boolean;
+    protected tenants: Person[];
+    constructor(protected key: Key) { }
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+            console.log("Welcome");
+        } else {
+            console.log("Key is not valid");
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +43,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
